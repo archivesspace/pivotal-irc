745f2155385648b1693cf680133b539c6862137d
Chris Beer <cabeer@stanford.edu>
2013-03-18 18:49:56 -0700

    karma

--- lib/karma.rb
+++ lib/karma.rb
@@ -1,68 +1,54 @@
 require 'cinch'
+require 'dbm'
+require 'thread'
+$karma_semaphore = Mutex.new
 
 class Karma
   include Cinch::Plugin
 
+ set :prefix, ""
+
+  attr_reader :database
+
   def initialize(*args)
     super
-    @scores_file = "karma.json"
-    if File.exist? @scores_file
-      File.open(@scores_file, "r") do |f|
-        @users = JSON.parse(f.read)
-        @users.default = 0
-      end
-    else
-      @users = Hash.new(0)
-    end
+
+    @database = DBM.open('karma', nil, DBM::WRCREAT)
   end
 
-  match /(\S+)[\+]{2}/, method: :increment
-  def increment(m, nick)
-    if nick == @bot.nick
-      m.reply "Increasing my karma would result in overflow."
-    elsif nick == m.user.nick
-      m.reply "Just keep patting yourself on the back there, sport."
-    else
-      update_user(nick) { |nick| @users[nick] += 1 }
-      show_score(m, nick)
-    end
+  match /(\S+)\+\+/, method: :increment
+  def increment(m, thing)
+    $karma_semaphore.synchronize {
+      @database[thing] = @database[thing].to_i + 1
+      @database[thing + "_inc"] = @database[thing + "_inc"].to_i + 1
+    }
   end
 
-  match /(\S+)[\-]{2}/, method: :decrement
-  def decrement(m, nick)
-    if nick == @bot.nick
-      m.reply "I wouldn't do that if I were you..."
-    elsif nick == m.user.nick
-      m.reply "There are special rooms on this network for self-flagellation."
-    else
-      update_user(nick) { |nick| @users[nick] -= 1 }
-      show_score(m, nick)
-    end
+  match /(\S+)--/, method: :decrement
+  def decrement(m, thing)
+     
+    $karma_semaphore.synchronize {
+      @database[thing] = @database[thing].to_i - 1
+      @database[thing + "_dec"] = @database[thing + "_dec"].to_i + 1
+    }
   end
 
-  match /karma\?(\s+)?(\S+)?/, method: :show_scores
-  def show_scores(m, cmd, nick, n=5)
-    if nick
-      show_score(m, nick)
+  match /^@karma?(\s+)?(\S+)?/, method: :show_scores
+  def show_scores(m, whitespace, thing, n=5)
+    if thing
+      m.reply "Karma for #{thing} has been increased #{@database[thing + "_inc"]} times and decreased #{@database[thing + "_dec"]} times for a total karma of #{@database[thing]}."
     else
-      sorted_users = @users.sort_by { |k, v| v }
-      top_scores   = sorted_users.take(n)
-      top_scores.each { |nick, score| show_score(m, nick) }
+      sorted_things = @database.reject { |k, v| k =~ /(_inc|_dec)$/} .to_hash.sort_by { |k, v| v }
+      top_scores   = sorted_things.take(n)
+      m.reply "Highest karma: #{top_scores.map { |thing, value| "#{thing} (#{value})" }.join(", ")}"
+      last_scores   = sorted_things.reverse.take(n).reverse
+      m.reply "Lowest karma: #{last_scores.map { |thing, value| "#{thing} (#{value})" }.join(", ")}"
     end
   end
   
-  def show_score(m, nick)
-    m.reply "#{ nick } has #{ @users[nick] } awesome points."
-  end
 
-  def update_user(nick)
-    yield(nick)
+  def update_user(thing)
+    yield(thing)
     save
-  end
-
-  def save
-    File.open(@scores_file, "w") do |f|
-      f.write(@users.to_json)
-    end
   end
 end
